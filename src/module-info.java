dd6587158eb92e1bf250561cf1d0d5abfb18d767
Anicet <tghil7@aol.com>
2020-09-20 22:07:33 -0500

    Module info update

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,9 @@
 module testFX {
 	requires javafx.controls;
 	requires javafx.base;
-	requires javafx.fxml;
 	requires javafx.graphics;
-	requires javafx.media;
-	requires javafx.swing;
-	requires javafx.web;
 	
 	
-	opens application to javafx.graphics, javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.controls,javafx.fxml;
 }
